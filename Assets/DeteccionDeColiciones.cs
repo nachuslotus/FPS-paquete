703dc22bb8872d22196b279922a46419cb809044
nachuslotus <166507478+nachuslotus@users.noreply.github.com>
2025-05-23 10:47:30 -0300

    Progresamos
    Creamos el prefab del barril, y le agregamos collider, rigidbody, mesh collider, mesh renderer y mesh filter. Tambien hicimos que cuando entraba en contacto con otro objeto, este desaparecia

---
+++ Assets/DeteccionDeColiciones.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColiciones : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(gameObject.gameObject);
+    }
+
+}
