c672989bcb8c142c0187a02348443d916070c081
nachuslotus <166507478+nachuslotus@users.noreply.github.com>
2025-05-26 09:06:28 -0300

    Colision y organizacion
    Hicimos la colision del player con los demas objetos, y ordenamos los objetos de la escena en carpetas, y vamos a comenzar con el score manager

--- Assets/DeteccionDeColiciones.cs
+++ Assets/DeteccionDeColiciones.cs
@@ -7,8 +7,12 @@ public class DeteccionDeColiciones : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Contacto");
-        Destroy(gameObject.gameObject);
+        Debug.Log("Contacto con" + collision.gameObject.name);
+
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
