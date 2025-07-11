35afd49f9e5d2097ddc1e6517df8629403ba27c9
nachuslotus <166507478+nachuslotus@users.noreply.github.com>
2025-07-11 10:40:19 -0300

    Cubos de dinero añadidos
    Añadi cubos de dinero al igual que los cubos de vida con el mismo script, solo que le cambie el Healthmanager por moneymanager. Gero evaluo mi parkour y me saque un 9.

---
+++ Assets/Scripts/MoneyPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyPoint : MonoBehaviour
+{
+    public UImanager UImanager;
+    public MoneyManager MoneyManager;
+    public int dinero = 100;
+
+    void Start()
+    {
+        MoneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (MoneyManager.UpdateMoney(dinero))
+            {
+                Destroy(gameObject);
+            }
+
+
+        }
+    }
+}
